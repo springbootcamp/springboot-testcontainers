271213236d18a016b2f95afdbe8349f8036fa9b1
Jan Galinski <jan.galinski@holisticon.de>
2019-11-12 07:27:18 +0100

    remove plugins

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,8 +15,4 @@ kotlinDslPluginOptions {
 
 dependencies {
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$embeddedKotlinVersion")
-
-
-  implementation("org.jetbrains.dokka:dokka-gradle-plugin:0.10.0")
-  implementation("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.4")
 }
